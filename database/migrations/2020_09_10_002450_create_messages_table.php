0dc8d005cb41cf106b9f119888bd94c89cc97d67
ahmedhassanien503 <ahm_hassanien@yahoo.com>
2020-09-10 16:34:46 +0200

    project

---
+++ database/migrations/2020_09_10_002450_create_messages_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('Message_Content');
+            $table->timestamps();
+            $table->foreignId('room_id')
+            ->references('id')
+            ->on('rooms')
+            ->onDelete('cascade');
+
+            $table->foreignId('user_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
